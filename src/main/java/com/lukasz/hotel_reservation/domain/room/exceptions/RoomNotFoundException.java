e9106b22c4846e1966dafb0f62941b6db2658d4d
Łukasz <luxus0@gmail.com>
2025-03-21 22:41:45 +0100

    Refactor

--- src/main/java/com/lukasz/hotel_reservation/domain/room/exceptions/RoomNotFoundException.java
+++ src/main/java/com/lukasz/hotel_reservation/domain/room/exceptions/RoomNotFoundException.java
@@ -1,7 +1,9 @@
 package com.lukasz.hotel_reservation.domain.room.exceptions;
 
+import java.util.UUID;
+
 public class RoomNotFoundException extends RuntimeException{
-    public RoomNotFoundException(){
-        super("Room not found");
+    public RoomNotFoundException(UUID roomId){
+        super("Room ID: " + roomId + " not found");
     }
 }
