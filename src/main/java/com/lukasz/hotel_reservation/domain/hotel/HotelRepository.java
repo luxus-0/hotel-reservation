8674cbd00e440611e15e99fd419effacb247e185
Łukasz <luxus0@gmail.com>
2025-03-23 23:59:54 +0100

    Remove unused class

--- src/main/java/com/lukasz/hotel_reservation/domain/hotel/HotelRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lukasz.hotel_reservation.domain.hotel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
--- src/main/java/com/lukasz/hotel_reservation/domain/hotel/exceptions/HotelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lukasz.hotel_reservation.domain.hotel.exceptions;
-
-public class HotelNotFoundException extends RuntimeException {
-    public HotelNotFoundException() {
-        super("Hotel not found");
-    }
-}
