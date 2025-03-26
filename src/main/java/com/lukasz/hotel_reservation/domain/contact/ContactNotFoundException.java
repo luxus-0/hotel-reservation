2993968ff0679ad1a7ebcb489b30f27802714371
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-03-26 20:12:06 +0100

    Refactor

--- src/main/java/com/lukasz/hotel_reservation/domain/contact/ContactNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lukasz.hotel_reservation.domain.contact;
-
-public class ContactNotFoundException extends RuntimeException {
-    public ContactNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/lukasz/hotel_reservation/domain/pdf/dto/TableRequest.java
+++ src/main/java/com/lukasz/hotel_reservation/domain/pdf/dto/TableRequest.java
@@ -1,9 +1,6 @@
-package com.lukasz.hotel_reservation.domain.pdf;
+package com.lukasz.hotel_reservation.domain.pdf.dto;
 
-import com.lukasz.hotel_reservation.domain.pdf.dto.CellRequest;
 import lombok.Builder;
-
-import java.util.List;
 
 @Builder
 public record TableRequest(int numberOfColumns,
