6b43aa9e8c3005990821fb8261f4273c31d28051
luxus-0 <nowogorski.lukasz0@gmail.com>
2025-04-11 19:36:46 +0200

    Refactor Payment entity

--- src/main/java/com/lukasz/hotel_reservation/domain/payment/Payment.java
+++ src/main/java/com/lukasz/hotel_reservation/domain/payment/Payment.java
@@ -1,43 +1,25 @@
 package com.lukasz.hotel_reservation.domain.payment;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payment")
 public class Payment {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private PaymentStatus status;
     private PaymentMethod method;
     private LocalDate date;
     private BigDecimal amount;
-
-    public Payment() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public PaymentStatus getStatus() {
-        return status;
-    }
-
-    public PaymentMethod getMethod() {
-        return method;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-
 }
